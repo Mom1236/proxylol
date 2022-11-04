2a601ab4ffc084feaed4d7bb2c4adea44252be3d
Mom1236 <89107436+Mom1236@users.noreply.github.com>
2022-11-04 17:16:26 -0500

    Update index.js

--- Site/index.js
+++ Site/index.js
@@ -19,7 +19,3 @@ function isUrl(val = ''){
     if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
     return false;
 };
-
-function quickLink(url1) {
-    window.location.href = __uv$config.prefix + url1;
-}
